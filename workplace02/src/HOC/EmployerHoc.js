cf6479ac3e98c09d83509206c643e412f8b19cb9
Prashant Mishra <prashantmishra111@outlook.com>
2022-12-25 20:05:05 +0530

    HOC Nav for candidate and Employer added!!!

---
+++ workplace02/src/HOC/EmployerHoc.js
@@ -0,0 +1,176 @@
+import * as React from "react";
+import {
+  AppBar,
+  Box,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  Container,
+  Button,
+  MenuItem,
+  Tooltip,
+} from "@mui/material";
+import Logo from "../../src/assets/workPlaceLogo.png";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useNavigate } from "react-router-dom";
+const pages = [
+    {
+      name: "jobs",
+      path: "/employer/jobs",
+    },
+    {
+      name: "Applicants",
+      path: "/employer/applications",
+    },
+    {
+      name:'Conversation',
+      path:'/employer/conversations'
+    },
+    {
+      name:'Profile',
+      path:'/employer/profile'
+    },
+  
+  ];
+  const settings = ["Profile", "Account", "Dashboard", "Logout"];
+
+function EmployerHoc(  {children} ) {
+  const navigate = useNavigate();
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleNavigate = (path) => {
+    navigate(`${path}`);
+  };
+
+  return (
+    <>
+    
+    <AppBar position="sticky" sx={{ background: "#ffff " }}>
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box
+            sx={{
+              width: "auto",
+              display: { xs: "none", md: "flex" },
+              //    border: "1px solid red"
+              paddingTop: "20px",
+            }}
+          >
+            <img
+              src={Logo}
+              alt="logo"
+              style={{ maxWidth: "100px", width: "100%" }}
+            />
+          </Box>
+
+          <Box sx={{ flexGrow: 1, display: { xs: "flex", md: "none" } }}>
+            <IconButton
+              size="large"
+              aria-label="account of current user"
+              aria-controls="menu-appbar"
+              aria-haspopup="true"
+              onClick={handleOpenNavMenu}
+              color="black"
+            >
+              <MenuIcon />
+            </IconButton>
+            <Menu
+              id="menu-appbar"
+              anchorEl={anchorElNav}
+              anchorOrigin={{
+                vertical: "bottom",
+                horizontal: "left",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "left",
+              }}
+              open={Boolean(anchorElNav)}
+              onClose={handleCloseNavMenu}
+              sx={{
+                display: { xs: "block", md: "none" },
+              }}
+            >
+              {pages.map((page) => (
+                <MenuItem
+                  key={page.name}
+                  onClick={() => handleNavigate(page.path)}
+                >
+                  <Typography textAlign="center">{page.name}</Typography>
+                </MenuItem>
+              ))}
+            </Menu>
+
+            <Box
+              sx={{
+                width: "auto",
+
+                //    border: "1px solid red"
+                paddingTop: "20px",
+              }}
+            >
+              <img
+                src={Logo}
+                alt="logo"
+                style={{ maxWidth: "100px", width: "100%" }}
+              />
+            </Box>
+          </Box>
+
+          <Box
+            sx={{
+              flexGrow: 1,
+              display: { xs: "none", md: "flex" },
+              justifyContent: "center",
+            }}
+          >
+            {pages.map((page) => (
+              <Button
+                key={page.name}
+                onClick={() => handleNavigate(page.path)}
+                sx={{
+                  my: 2,
+                  color: "black",
+                  display: "block",
+                  padding: "0 2rem",
+                }}
+              >
+                {page.name}
+              </Button>
+            ))}
+          </Box>
+
+          <Box sx={{ flexGrow: 0 }}>
+            <Tooltip>
+              <Button onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+                <Typography sx={{ color: "black" }}>Dark Mode</Typography>
+              </Button>
+            </Tooltip>
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+    {children}
+    </>
+  );
+}
+
+export default EmployerHoc;
--- workplace02/src/Navs/index.js
+++ workplace02/src/Navs/index.js
@@ -1,103 +1,99 @@
-import React from 'react';
-import { BrowserRouter as Router, Navigate, Outlet, Route, Routes } from "react-router-dom";
-import AuthPage from '../Components/pages/AuthPage';
-import LandingPage from '../Components/LandingPage';
-import CandidateApplications from '../Components/pages/candidate/CandidateApplications';
-import CandidateConversation from '../Components/pages/candidate/CandidateConversation';
-import CandidateJobs from '../Components/pages/candidate/CandidateJobs';
-import CandidateOnboarding from '../Components/pages/candidate/CandidateOnboarding';
-import CandidateProfile from '../Components/pages/candidate/CandidateProfile';
-import EmployerApplications from '../Components/pages/employer/EmployerApplications';
-import EmployerConversations from '../Components/pages/employer/EmployerConversations';
-import EmployerJobs from '../Components/pages/employer/EmployerJobs';
-import EmployerOnboarding from '../Components/pages/employer/employerOnboarding/index';
-import EmployerProfile from '../Components/pages/employer/EmployerProfile';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Outlet,
+  Route,
+  Routes,
+} from "react-router-dom";
+import AuthPage from "../Components/pages/AuthPage";
+import LandingPage from "../Components/LandingPage";
+import CandidateApplications from "../Components/pages/candidate/CandidateApplications";
+import CandidateConversation from "../Components/pages/candidate/CandidateConversation";
+import CandidateJobs from "../Components/pages/candidate/CandidateJobs";
+import CandidateOnboarding from "../Components/pages/candidate/candidateOnboarding/index";
+import CandidateProfile from "../Components/pages/candidate/CandidateProfile";
+import EmployerApplications from "../Components/pages/employer/EmployerApplications";
+import EmployerConversations from "../Components/pages/employer/EmployerConversations";
+import EmployerJobs from "../Components/pages/employer/EmployerJobs";
+import EmployerOnboarding from "../Components/pages/employer/employerOnboarding/index";
+import EmployerProfile from "../Components/pages/employer/EmployerProfile";
+import CandidateHoc from "../HOC/CandidateHoc";
+ 
 
 function Navs() {
-
-    const ProtectedCandidateRoute = () => {
-        if (true) {
-            return <Outlet />
-        }
-        else {
-            return <Navigate to="./candidate/auth" />
-        }
+  const ProtectedCandidateRoute = () => {
+    if (true) {
+      return <Outlet />;
+    } else {
+      return <Navigate to="./candidate/auth" />;
     }
-    const ProtectedEmployerRoute = () => {
-        if (true) {
-            return <Outlet />
-        }
-        else {
-            return <Navigate to="./employer/auth" />
-        }
+  };
+  const ProtectedEmployerRoute = () => {
+    if (true) {
+      return <Outlet />;
+    } else {
+      return <Navigate to="./employer/auth" />;
     }
+  };
 
-    return (
-        <Router>
-            <Routes >
-                <Route path='/' element={<LandingPage />} />
-                {/* <Route path='/auth' element={<AuthPage />} /> */}
-                {/* candidate route */}
-                <Route path='/candidate/auth' element={<AuthPage  type="candidate" />} />
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        {/* <Route path='/auth' element={<AuthPage />} /> */}
+        {/* candidate route */}
+        <Route path="/candidate/auth" element={<AuthPage type="candidate" />} />
 
-                {/* we dont want user to access the Candidate profile, Candidate jobs,Candidate application and Candidate conversations section without login */}
-                {/* to achieve this functionality we are going to use   PROTECTED ROUTE */}
-                <Route element={<ProtectedCandidateRoute />}>
-                    <Route
-                        path='/candidate/onboarding'
-                        element={<CandidateOnboarding />}
-                    />
-                    <Route
-                        path='/candidate/profile'
-                        element={<CandidateProfile />}
-                    />
-                    <Route
-                        path='/candidate/jobs'
-                        element={<CandidateJobs />}
-                    />
-                    <Route
-                        path='/candidate/applications'
-                        element={<CandidateApplications />}
-                    />
-                    <Route
-                        path='/candidate/conversations'
-                        element={<CandidateConversation />}
-                    />
+        {/* we dont want user to access the Candidate profile, Candidate jobs,Candidate application and Candidate conversations section without login */}
+        {/* to achieve this functionality we are going to use   PROTECTED ROUTE */}
+        <Route element={<ProtectedCandidateRoute />}>
+          <Route
+            path="/candidate/onboarding"
+            element={<CandidateOnboarding />}
+          />
+          <Route path="/candidate/profile" element={<CandidateHoc><CandidateProfile /></CandidateHoc> } />
+          <Route path="/candidate/jobs" element={<CandidateHoc><CandidateJobs /></CandidateHoc> } />
+          <Route
+            path="/candidate/applications"
+            element={<CandidateHoc><CandidateApplications /></CandidateHoc> }
+          />
+          <Route
+            path="/candidate/conversations"
+            element={<CandidateHoc><CandidateConversation /></CandidateHoc> }
+          />
+        </Route>
 
-                </Route>
+        {/* employer route */}
+        <Route path="/employer/auth" element={<AuthPage type="employer" />} />
 
-                {/* employer route */}
-                <Route path='/employer/auth' element={<AuthPage type="employer" />} />
+        {/* we dont want user to access the employer profile, employer   jobs,employer application and employer conversations section without login */}
+        {/* to achieve this functionality we are going to use   PROTECTED ROUTE */}
 
-                 {/* we dont want user to access the employer profile, employer   jobs,employer application and employer conversations section without login */}
-                {/* to achieve this functionality we are going to use   PROTECTED ROUTE */}
-                
+        <Route element={<ProtectedEmployerRoute />}>
+          <Route path="/employer/onboarding" element={<EmployerOnboarding />} />
 
-                <Route element={<ProtectedEmployerRoute />}>
-                    <Route
-                        path='/employer/onboarding'
-                        element={<EmployerOnboarding />}
-                    />
-                    <Route
+          <Route
                         path='/employer/profile'
-                        element={<EmployerProfile />}
+                        element={<CandidateHoc> <EmployerProfile /> </CandidateHoc>}
                     />
                     <Route
                         path='/employer/jobs'
-                        element={<EmployerJobs />}
+                        element={<CandidateHoc><EmployerJobs /></CandidateHoc> }
                     />
                     <Route
                         path='/employer/applications'
-                        element={<EmployerApplications />}
+                        element={<CandidateHoc><EmployerApplications /></CandidateHoc> }
                     />
                     <Route
                         path='/employer/conversations'
-                        element={<EmployerConversations />}
+                        element={<CandidateHoc><EmployerConversations /></CandidateHoc>  }
                     />
-                </Route>
-            </Routes>
-        </Router>
-    )
+           
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default Navs;
