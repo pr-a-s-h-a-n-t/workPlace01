7585b4f17411b89c46a1b8cd59f25c437abbc9d4
Prashant Mishra <prashantmishra111@outlook.com>
2023-01-01 20:14:01 +0530

    candidate conversations page done, some styling remaining!!!

--- workplace02/src/Components/pages/candidate/candidateConversations/index.js
+++ workplace02/src/Components/pages/candidate/candidateConversations/index.js
@@ -1,11 +1,100 @@
-import React from 'react'
+import { Grid } from "@mui/material";
+import { collection, onSnapshot, query, where } from "firebase/firestore";
+import { db } from "../../../../firebaseConfig/index";
+import React, { useEffect, useState } from "react";
+import MessageArea from "./messageArea/index";
+import SideBar from "./sidebar/index";
 
-function CandidateConversation() {
+function EmployerConversation() {
+  const [allConversations, setAllConversations] = useState(null);
+  const [selectedSectionMobile, setSelectedSectionMobile] = useState("sidebar");
+  const [currentSelectedMessage, setCurrentSelectedMessage] = useState(null);
+  const handleClick = async (message) => {
+    console.log(message);
+    setCurrentSelectedMessage(message);
+    // fetch all the docs from the conversation collection,
+    //  where the conversation id is equal to the
+    //  conversation id of the message
+    // subscribe to it
+
+    const q = query(
+      collection(db, "conversations"),
+      where("conversation_id", "==", message.conversation_id)
+    );
+    const unsubscribe = await onSnapshot(q, (querySnapshot) => {
+      let docs = [];
+      querySnapshot.forEach((doc) => {
+        docs.push(doc.data());
+       
+
+      });
+      setAllConversations(docs);
+       
+    });
+  };
+
+  useEffect(() => {
+    if (allConversations) {
+      setSelectedSectionMobile("messageArea");
+    }
+  }, [allConversations]);
+
   return (
-    <div>
-      this is CandidateConversation Page!!!@
-    </div>
-  )
+    <Grid
+      container
+      spacing={2}
+      sx={{
+        maxWidth: "100%",
+        height: "85vh",
+        paddingTop: "1rem  ",
+      }}
+    >
+      <Grid
+        sx={{
+          display: {
+            xs: selectedSectionMobile === "sidebar" ? "block" : "none",
+            md: "block",
+          },
+        }}
+        item
+        xs={12}
+        md={3}
+      >
+        <SideBar
+          currentSelectedMessage={currentSelectedMessage}
+          handleClick={handleClick}
+        />
+      </Grid>
+      <Grid
+        sx={{
+          display: {
+            xs: selectedSectionMobile === "messageArea" ? "block" : "none",
+            md: "block",
+          },
+          marginTop: "2rem",
+          height: "94%",
+          overflowY: "auto",
+          overflowX: "hidden",
+          // backgroundColor: "",
+           borderLeft: "10px solid black",
+          boxShadow:"10px 5px 5px green",
+          boxSizing: "border-box",
+          overflowWrap: "break-word",
+        }}
+        item
+        xs={12}
+        md={9}
+      >
+        {currentSelectedMessage && (
+          <MessageArea
+            setSelectedSectionMobile={setSelectedSectionMobile}
+            currentSelectedMessage={currentSelectedMessage}
+            allConversations={allConversations}
+          />
+        )}
+      </Grid>
+    </Grid>
+  );
 }
 
-export default CandidateConversation
+export default EmployerConversation;
